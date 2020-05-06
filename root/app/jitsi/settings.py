e4c1eb97f0bca6e57051217f07211f36bfa1f192
Simora Altreus <simora@simora.haus.marklehaus.com>
2020-05-06 16:36:14 -0400

    Fix for collectstatic

--- root/app/jitsi/settings.py
+++ root/app/jitsi/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = BASE_DIR + '/static'
 STATIC_URL = '/static/'
 
 LOGGING = {
